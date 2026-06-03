ece7fbf87333bb7689aa672803a74b4260e5df4d
Aleazar Wolde <aleazarali123@gmail.com>
2026-06-03 18:33:54 -0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/gatewaykit/gatewaykit/GatewaykitApplication.java
@@ -0,0 +1,13 @@
+package com.gatewaykit.gatewaykit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaykitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaykitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatewaykit/gatewaykit/GatewaykitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewaykit.gatewaykit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaykitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
